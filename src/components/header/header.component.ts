983ba1047308c40c3aa2282a3f9bedf0ae25e910
Emmon Moses <oemmon@yahoo.com>
2020-06-15 09:24:24 +0300

    homepage images update

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -36,5 +36,9 @@ export class HeaderComponent implements OnInit {
     });
 
   }
-
+  // // When the user clicks on the button, scroll to the top of the document
+  topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 }
