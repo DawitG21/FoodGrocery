0ae3b180b377bd9b811087f54f4c06ccbf8d7aa0
Emmon Moses <oemmon@yahoo.com>
2020-06-24 16:52:20 +0300

    excess padding/margin on mobile view removed.scrolling on navigation added.

--- src/components/index/index.component.ts
+++ src/components/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+/* import { Router } from '@angular/router'; */
 
 @Component({
   selector: 'app-index',
@@ -7,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    /* public route: Router */
+    ) { }
 
   ngOnInit() { }
+
+  topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    /* this.route.navigate(['/faq']); */
+  }
 
 }
