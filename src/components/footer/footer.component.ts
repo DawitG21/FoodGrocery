252b090646d63eebbcb8e5903f8652d59f781f4c
Emmon Moses <oemmon@yahoo.com>
2020-08-23 14:16:10 +0300

    fix footer

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core';
-import { APP_BASE_HREF, Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,41 +7,11 @@ import { APP_BASE_HREF, Location } from '@angular/common';
 })
 export class FooterComponent implements OnInit {
 
-  languages = [
-    { code: 'en-US', label: 'English' },
-    { code: 'am', label: 'Amharic' },
-  ];
-
   constructor(
-    @Inject(LOCALE_ID) public localeId: string,
-    @Inject(APP_BASE_HREF) public baseHref: string,
-    public location: Location
-  ) {
-    // this.cacheLocalePreference();
-  }
+
+  ) { }
 
   ngOnInit() { }
 
-  // // When the user clicks on the button, scroll to the top of the document
-  topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-
-  cacheLocalePreference() {
-    if (this.localeId === 'en-US') {
-      localStorage.removeItem('amPath');
-      localStorage.setItem('enPath', location.href);
-      // console.log('localeID', this.localeId);
-      // console.log('baseHref', this.baseHref);
-      // console.log('baseHref', this.location);
-    } else if (this.localeId === 'am') {
-      localStorage.removeItem('enPath');
-      localStorage.setItem('amPath', location.href);
-      // console.log('localeID', this.localeId);
-      // console.log('baseHref', this.baseHref);
-      // console.log('baseHref', this.location);
-    }
-  }
 
 }
